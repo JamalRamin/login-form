826b20840c8d5a1ea24f2c1ca897486e9dbcb152
Ramin jamal <131708198+jamalramin@users.noreply.github.com>
2023-07-23 22:49:31 +0430

    Add files via upload

---
+++ style.js
@@ -0,0 +1,19 @@
+function showsss() {
+  var s = document.getElementById("signUp");
+  s.style.transitionDuration = "1s";
+  s.style.transform = "scale(1)";
+
+  var f = document.getElementById("f");
+  f.style.transitionDuration = "1s";
+  f.style.transform = "scale(0)";
+}
+
+function showfff() {
+  var f = document.getElementById("f");
+  f.style.transitionDuration = "1s";
+  f.style.transform = "scale(1)";
+
+  var s = document.getElementById("signUp");
+  s.style.transitionDuration = "1s";
+  s.style.transform = "scale(0)";
+}
